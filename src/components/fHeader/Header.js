a5841ecd33247c42981afe0bb724b9e09c9f7053
Alexandra Gutierrez <162755566+0Kelly087@users.noreply.github.com>
2024-10-06 15:31:55 +0000

    UPGRADE: header details

--- src/components/fHeader/Header.js
+++ src/components/fHeader/Header.js
@@ -1,31 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "../../styles/sHome/Header.module.scss";
-import { useRouter } from "next/router"; // Importa useRouter para cambiar de página
 
 const Header = () => {
-  const [welcomeScreen, setWelcomeScreen] = useState(true);
-  const router = useRouter(); // Instancia de useRouter
-
-  const handleContinue = () => {
-    router.push("/next-page"); // Cambia de página al hacer clic en "Continuar"
-  };
-
   return (
-    <section className={styles.globalHeader}>
-      {welcomeScreen ? (
-        <div className={styles.welcomeScreen}>
-          <h1 className={styles.welcomeText}>Welcome to the Landsat Tracking System</h1>
-          <button className={styles.continueButton} onClick={handleContinue}>
-            Continue
-          </button>
+    <header className={styles.header}>
+      <section className={styles.section}>
+        <h1>RogerNatividad</h1>
+        <div className={styles.conte}>
+          <p>Bienvenido al Tutorial</p>
+          <a href="#">Crear Cuenta</a>
+          <a href="#">Ingresar</a>
         </div>
-      ) : (
-        <div className={styles.nextScreen}>
-          <h2>Second Screen</h2>
-          <p>Now you are on the second page of the Landsat Tracking System.</p>
-        </div>
-      )}
-    </section>
+      </section>
+    </header>
   );
 };
 
