03b87e6fb0ebebc03360d15792924963b9d930cc
David Chaves Gutierrez <142677288+DAID04@users.noreply.github.com>
2024-10-07 04:06:51 +0000

    New Header

--- src/components/fHeader/Header.tsx
+++ src/components/fHeader/Header.tsx
@@ -1,17 +1,27 @@
 import React from "react";
 import styles from "./Header.module.scss";
+import Link from 'next/link';
+import Image from 'next/image';
+import logo from "../public/Logo.png";
 
-interface HeaderProps {
-  className?: string;
-}
-
-const Header: React.FC<HeaderProps> = ({ className }) => {
+const Header: React.FC = () => {
   return (
-    <header className={`${styles.globalHeader} ${className}`}>
+    <header className={styles.globalHeader}>
       <section>
-        <h1 className={styles.title}>SPACE APP CHALLENGE</h1>
+        <Link href="/">
+          <Image
+            src={logo}
+            alt="Logo"
+            width={100}
+            height={50}
+            className={styles.Logo}
+          />
+        </Link>
         <div className={styles.container}>
-          <p>Welcome LANDSTAT</p>
+          <h1 className={styles.title}>SPACE APP CHALLENGE</h1>
+          <div className={styles.nextScreen}>
+            <p>Welcome LANDSTAT</p>
+          </div>
         </div>
       </section>
     </header>
@@ -19,4 +29,3 @@ const Header: React.FC<HeaderProps> = ({ className }) => {
 };
 
 export default Header;
-
